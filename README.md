e33e3322affb9ff3f3abe072fcaeaef2ea434378
Nacho Caballero <nachocab@gmail.com>
2017-09-04 18:16:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# highlight-dodgy-characters README
+
+Highlight all non-unicode characters (port of Highlight Dodgy Characters from Sublime Text)
+
+## Features
+
+It highlights all the non-unicode characters, including dashes, fancy quotes and emojis. 
+
+Inspired from [Highlight Bad Chars](https://github.com/WengerK/vscode-highlight-bad-chars/)
---
+++ extension.js
@@ -0,0 +1,62 @@
+const vscode = require("vscode");
+
+function activate(context) {
+  const badCharDecorationType = vscode.window.createTextEditorDecorationType({
+    cursor: 'crosshair',
+    backgroundColor: 'rgba(255,0,0,0.3)',
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: 'rgba(255,0,0,0.6)'
+  });
+
+  const whitelist = '\n´\u0009' // allow newline, forward-tick and tabulator
+
+  // search for non-ascii characters that are not on the whitelist
+  const charRegExp = `[^\x00-\x7F${whitelist}]`
+
+  let editor = vscode.window.activeTextEditor;
+  if (editor) {
+    triggerUpdateDecorations();
+  }
+
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+    editor = editor;
+    if (editor) {
+      triggerUpdateDecorations();
+    }
+  }, null, context.subscriptions);
+
+  vscode.workspace.onDidChangeTextDocument(event => {
+    if (editor && event.document === editor.document) {
+      triggerUpdateDecorations();
+    }
+  }, null, context.subscriptions);
+  
+  
+  var timeout = null;
+  function triggerUpdateDecorations() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(updateDecorations, 500);
+  }
+
+  function updateDecorations() {
+    if (!editor) {
+      return;
+    }
+    let regEx = new RegExp(charRegExp, 'g');
+    const text = editor.document.getText();
+    const badChars = [];
+    let match;
+    while (match = regEx.exec(text)) {
+      const startPos = editor.document.positionAt(match.index);
+      const endPos = editor.document.positionAt(match.index + match[0].length);
+      const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Bad char "**' + match[0] + '**"' };
+      badChars.push(decoration);
+    }
+    editor.setDecorations(badCharDecorationType, badChars);
+  }
+}
+exports.activate = activate;
+//# sourceMappingURL=extension.js.map
